ebc3421ea5ecd99e98622b7d8c1ab7995e43a667
Mohammad-Anar <ai.anarul4232@gmail.com>
2026-02-24 23:17:05 +0600

    nav suer added from api

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -22,16 +22,11 @@ import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 import { useDispatch } from "react-redux";
 import { setAccessToken, setUser } from "@/redux/features/auth";
+import { useAppSelector } from "@/redux/hooks/hooks";
+import { RootState } from "@/redux/store";
 
-export function NavUser({
-  user,
-}: {
-  user: {
-    name: string;
-    email: string;
-    avatar: string;
-  };
-}) {
+export function NavUser() {
+  const user = useAppSelector((state: RootState) => state.auth.user);
   const { isMobile } = useSidebar();
   const dispatch = useDispatch();
 
@@ -60,7 +55,10 @@ export function NavUser({
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <Avatar className="h-10 w-10 rounded-lg ">
-                <AvatarImage src={user?.avatar} alt={user?.name || "User"} />
+                <AvatarImage
+                  src={user?.profilePhoto}
+                  alt={user?.name || "User"}
+                />
                 <AvatarFallback className="rounded-lg">CN</AvatarFallback>
               </Avatar>
               {/* <div className="grid flex-1 text-left text-sm leading-tight">
@@ -83,7 +81,7 @@ export function NavUser({
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
                 <Avatar className="h-8 w-8 rounded-lg">
-                  <AvatarImage src={user?.avatar} alt={user?.name} />
+                  <AvatarImage src={user?.profilePhoto} alt={user?.name} />
                   <AvatarFallback className="rounded-lg">CN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
